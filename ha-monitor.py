4e4ba60ca51d223f7b8a8af28b20401794d4a665
victor.fedorov <victor.fedorov@wirenboard.com>
2025-09-05 13:34:56 +0300

    [add] logger

--- ha-monitor.py
+++ ha-monitor.py
@@ -1,51 +1,85 @@
 #!/usr/bin/env python3
 
 import subprocess
-from datetime import datetime
-
+import sys
 import paho.mqtt.client as mqtt
+import logging
+
+# Настройки сервисов хранить в /etc/ИМЯ_СЕРВИСА.conf в формате JSON.
 
 TOPIC_NAME = "homeassistant/status"
-broker_ip = "localhost"
-broker_port = 1883
+BROKER_IP = "localhost"
+BROKER_PORT = 1883
+
+# setup logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s",
+    handlers=[logging.StreamHandler(sys.stdout)],
+    stream=sys.stdout,
+)
+
+logger = logging.getLogger(__name__)
 
 
 class SimpleHAStatusMonitor:
-    def __init__(self, broker=broker_ip, port=broker_port):
+    # Use actual callback API (v2)
+    def __init__(
+        self,
+        broker=BROKER_IP,
+        port=BROKER_PORT,
+    ):
         self.broker = broker
         self.port = port
         self.current_status = None
         self.previous_status = None
 
-    def on_connect(self, client, userdata, flags, reason_code, properties):
+    def on_connect(
+        self,
+        client,
+        userdata,
+        flags,
+        reason_code,
+        properties,
+    ):
         if reason_code == 0:
             result = client.subscribe(TOPIC_NAME)
             if result[0] == 0:
-                print(f"Subscribed to {TOPIC_NAME}")
+                logger.info(f"Subscribed to {TOPIC_NAME}")
             else:
-                print(f"Failed subscribe to {TOPIC_NAME}")
-            print("✓ await status for Home Assistant...")
+                logger.error(f"Failed subscribe to {TOPIC_NAME}")
         else:
-            print(
-                f"✗ Connection error: {reason_code} - {mqtt.error_string(reason_code)}"
+            logger.error(
+                f"Connection error: {reason_code} - {mqtt.error_string(reason_code)}"
             )
 
-    def on_message(self, client, userdata, msg):
+    def on_message(
+        self,
+        client,
+        userdata,
+        msg,
+    ):
         if msg.topic == TOPIC_NAME:
             new_status = msg.payload.decode().strip().lower()
-            timestamp = datetime.now().strftime("%H:%M:%S")
             # Save prev status
             self.previous_status = self.current_status
             self.current_status = new_status
-            print(f"[{timestamp}] Status: {new_status}")
+            logger.info(f"Status: {new_status}")
             # Check status "online"
             if (not self.previous_status and self.current_status == "online") or (
                 self.previous_status == "offline" and self.current_status == "online"
             ):
                 self.wb_engine_start()
 
-    def on_disconnect(self, client, *args):
-        print("Connection lost")
+    def on_disconnect(
+        self,
+        client,
+        userdata=None,
+        disconnect_flags=None,
+        reason_code=None,
+        properties=None,
+    ):
+        logger.error("Connection lost")
 
     def wb_engine_start(self):
         """
@@ -53,17 +87,17 @@ class SimpleHAStatusMonitor:
         :return:
         """
         try:
-            print("Try to start wb-engine-helper...")
+            logger.info("Try to start wb-engine-helper...")
             res = subprocess.run(
                 ["wb-engine-helper", "--start"],
                 capture_output=True,
                 text=True,
             )
-            print(res.stdout)
+            logger.warning(res.stdout)
 
         except subprocess.CalledProcessError as e:
-            print(f"Command failed with exit code {e.returncode}")
-            print(f"Stderr: {e.stderr.decode()}")
+            logger.error(f"Command failed with exit code {e.returncode}")
+            logger.error(f"Stderr: {e.stderr.decode()}")
 
     def start(self):
         # Use actual callback API (v2)
@@ -76,7 +110,7 @@ class SimpleHAStatusMonitor:
             client.connect(self.broker, self.port, 60)
             client.loop_forever()
         except Exception as e:
-            print(f"Error: {e}")
+            logger.error(f"Error: {e}")
 
 
 # Starting monitoring
